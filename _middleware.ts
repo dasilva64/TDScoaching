8cc4d6d3d17ad11b0c4a0e0ed16d9403ad8b26bd
thomasop <tdss33@hotmail.com>
2023-08-04 08:08:11 +0200

    add

--- _middleware.ts
+++ _middleware.ts
@@ -12,28 +12,28 @@ export const middleware = async (req: NextRequest) => {
     },
   });
   const { user } = session;
-  /* if (user) {
+  if (user) {
     if (
-      req.nextUrl.pathname === ("/utilisateurs") ||
-      req.nextUrl.pathname === ("/meetings") ||
-      req.nextUrl.pathname === ("/meetingAdmin")
+      req.nextUrl.pathname.startsWith("/utilisateurs") ||
+      req.nextUrl.pathname.startsWith("/meetings") ||
+      req.nextUrl.pathname.startsWith("/meetingAdmin")
     ) {
       if (user.role !== "ROLE_ADMIN") {
         return NextResponse.redirect(new URL("/", req.url));
       }
     }
-    if (req.nextUrl.pathname === ("/rendez-vous")) {
+    if (req.nextUrl.pathname.startsWith("/rendez-vous")) {
       if (user.role !== "ROLE_USER") {
         return NextResponse.redirect(new URL("/", req.url));
       }
     }
-  } */
+  }
   if (!user) {
     return NextResponse.redirect(new URL("/", req.url));
   }
-  /* if (user.role !== "ROLE_USER" && user.role !== "ROLE_ADMIN") {
+  if (user.role !== "ROLE_USER" && user.role !== "ROLE_ADMIN") {
     return NextResponse.redirect(new URL("/", req.url));
-  } */
+  }
   return res;
 };
 

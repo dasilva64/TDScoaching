1029b881fd6d1ff9e9a19bf8f76d7be8946504c5
thomasop <tdss33@hotmail.com>
2023-08-04 07:30:01 +0200

    add

---
+++ lib/getRequestCookie.ts
@@ -0,0 +1,20 @@
+import { unsealData } from "iron-session";
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+
+interface IronSessionData {
+    user?: any
+}
+
+export async function getRequestCookie(
+    cookies: ReadonlyRequestCookies
+): Promise<IronSessionData | null> {
+    const cookieName = "test";
+    const foundCookie = cookies.get(cookieName);
+    if (!foundCookie) {
+        return null;
+    }
+    const { user } = await unsealData(foundCookie.value, {
+        password: "tesdfjklsjtesdfjktesdfjklsjdfljslkdfjlsjdflslqfdjkstlsjdfljslkdfjlsjdflslqfdjkstdfljslkdfjlsjdflslqfdjkst",
+        });
+    return user as unknown as IronSessionData;
+}

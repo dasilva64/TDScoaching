87aec68b85fd5a706b18d22eacfa61f9b13456e7
thomasop <tdss33@hotmail.com>
2023-08-05 07:55:41 +0200

    add

---
+++ pages/api/user/check.ts
@@ -0,0 +1,49 @@
+import { NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
+import { withIronSessionApiRoute } from "iron-session/next";
+import { Prisma } from "@prisma/client";
+
+export default withIronSessionApiRoute(
+  async function check(req: any, res: NextApiResponse) {
+    if (req.method === "GET") {
+      if (req.session.user) {
+        const user = await prisma.user.findUnique({
+          where: { id: req.session.user.id },
+        });
+        if (user === null) {
+          return res.status(400).json({
+            status: 400,
+            message: "L'utilisateur n'a pas été trouvé, veuillez réessayer",
+          });
+        } else {
+          return res.status(200).json({
+            status: 200,
+            body: {
+              id: user.id,
+              role: user.role,
+            },
+          });
+        }
+      } else {
+        return res.status(404).json({
+          status: 404,
+          message: "Vous n'êtes pas connecté, veuillez réessayer",
+          body: null,
+        });
+      }
+    } else {
+      return res.status(404).json({
+        status: 404,
+        message: "Une erreur est survenue, veuillez réessayer",
+      });
+    }
+  },
+  {
+    password:
+      "tesdfjklsjtesdfjktesdfjklsjdfljslkdfjlsjdflslqfdjkstlsjdfljslkdfjlsjdflslqfdjkstdfljslkdfjlsjdflslqfdjkst",
+    cookieName: "test",
+    cookieOptions: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  }
+);

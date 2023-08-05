b9ff86abc08755b17016ea42db4c42a7f878e81d
thomasop <tdss33@hotmail.com>
2023-08-05 07:35:15 +0200

    add

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,12 +1,20 @@
 import Content from "./display/Content";
 import { cookies } from "next/headers";
-import { getRequestCookie } from "../../../../lib/getRequestCookie";
+import { unsealData } from "iron-session";
 
 const Header = async () => {
-  const user = await getRequestCookie(cookies());
+  //const user = await getRequestCookie(cookies());
+  const cookieStore = cookies();
+  const encrytedSession = cookieStore.get("test")?.value;
+  const session = encrytedSession
+    ? await unsealData(encrytedSession, {
+        password:
+          "tesdfjklsjtesdfjktesdfjklsjdfljslkdfjlsjdflslqfdjkstlsjdfljslkdfjlsjdflslqfdjkstdfljslkdfjlsjdflslqfdjkst",
+      })
+    : null;
   return (
     <>
-      <Content userLog={user} />
+      <Content userLog={session} />
     </>
   );
 };

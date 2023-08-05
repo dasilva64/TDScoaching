5a364e04778f7ec3b7d035a24b28da73641bc8fd
thomasop <tdss33@hotmail.com>
2023-08-05 09:25:28 +0200

    add

---
+++ src/app/components/hook/user/useCheck.ts
@@ -0,0 +1,23 @@
+import useSWR from "swr";
+
+const fetchWithToken = async (url: string) => {
+  let response = await fetch(url);
+  let json = await response.json();
+  return json;
+};
+
+function useCheck() {
+  const { data, error, isLoading, mutate } = useSWR(
+    ["/api/user/check"],
+    ([url]) => fetchWithToken(url)
+  );
+
+  return {
+    data,
+    isLoading,
+    isError: error,
+    mutate: mutate,
+  };
+}
+
+export default useCheck;

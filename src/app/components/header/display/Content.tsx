3fa340e34f18122c1694843597d1feb743fbb49c
thomasop <tdss33@hotmail.com>
2023-09-12 19:27:28 +0200

    Edit

--- src/app/components/header/display/Content.tsx
+++ src/app/components/header/display/Content.tsx
@@ -48,6 +48,7 @@ const Content = () => {
   const [onHoverLink, setOnHoverLink] = useState<string | null>(pathname);
   const router = useRouter();
   const { cache } = useSWRConfig();
+  const { isLog } = useSelector((state: RootState) => state.auth);
   const { isMobile } = useSelector((state: RootState) => state.Mobile);
   /* useEffect(() => {
     const fetchCheckUser = async () => {
@@ -72,164 +73,200 @@ const Content = () => {
       }
     }
   }, [data, dispatch]); */
-  let content;
-  content = (
-    <>
-      <div className={styles.header__log}>
-        <div
-          onClick={() => {
-            setDisplayLogMenu(!displayLogMenu);
-            if (isActive) {
-              dispatch({ type: "menu/closeMenu" });
-            }
-          }}
-          className={styles.header__log__div}
-        ></div>
-        <>
-          <div
-            className={`${styles.header__log__container} ${
-              displayLogMenu === true
-                ? styles.header__log__container__show
-                : styles.header__log__container__hide
-            }`}
-          >
-            <ul
-              onMouseLeave={() => setOnHoverLink(pathname)}
-              className={`${styles.header__log__ul}`}
-            >
-              <li
-                onMouseOver={() => setOnHoverLink("/profile")}
-                className={`${styles.header__log__li} ${
-                  styles.header__log__li__1
-                } ${
-                  displayLogMenu === true
-                    ? styles.header__log__li__show
-                    : styles.header__log__li__hide
-                }`}
-              >
-                <Link
-                  className={styles.header__log__li__link}
-                  href="/profile"
-                  onClick={() => setDisplayLogMenu(false)}
-                >
-                  <div
-                    className={`${styles.header__log__li__point} ${
-                      onHoverLink === "/profile"
-                        ? styles.header__log__li__point__show
-                        : styles.header__log__li__point__hide
-                    }`}
-                  ></div>
-                  Compte
-                </Link>
-              </li>
-              <li
-                onMouseOver={() => setOnHoverLink("/historique")}
-                className={`${styles.header__log__li} ${
-                  styles.header__log__li__2
-                } ${
-                  displayLogMenu === true
-                    ? styles.header__log__li__show
-                    : styles.header__log__li__hide
-                }`}
-              >
-                <Link
-                  className={styles.header__log__li__link}
-                  href="/historique"
-                  onClick={() => setDisplayLogMenu(false)}
-                >
-                  <div
-                    className={`${styles.header__log__li__point} ${
-                      onHoverLink === "/historique"
-                        ? styles.header__log__li__point__show
-                        : styles.header__log__li__point__hide
-                    }`}
-                  ></div>
-                  Historique des rendez-vous
-                </Link>
-              </li>
-              <li
-                onMouseOver={() => setOnHoverLink("/rendez-vous")}
-                className={`${styles.header__log__li} ${
-                  styles.header__log__li__3
-                } ${
-                  displayLogMenu === true
-                    ? styles.header__log__li__show
-                    : styles.header__log__li__hide
-                }`}
-              >
-                <Link
-                  className={styles.header__log__li__link}
-                  href="/rendez-vous"
-                  onClick={() => setDisplayLogMenu(false)}
-                >
-                  <div
-                    className={`${styles.header__log__li__point} ${
-                      onHoverLink === "/rendez-vous"
-                        ? styles.header__log__li__point__show
-                        : styles.header__log__li__point__hide
-                    }`}
-                  ></div>
-                  Mes rendez-vous
-                </Link>
-              </li>
-              <li
-                onMouseOver={() => setOnHoverLink("/logout")}
-                className={`${styles.header__log__li} ${
-                  styles.header__log__li__4
-                } ${
-                  displayLogMenu === true
-                    ? styles.header__log__li__show
-                    : styles.header__log__li__hide
-                }`}
-              >
-                <span
-                  className={styles.header__log__li__link}
-                  onClick={() => {
-                    const logout = async () => {
-                      let response = await fetch("/api/user/logout");
-                      let json = await response.json();
-                      console.log(json);
-                      if (json && json.status === 200) {
-                        dispatch({
-                          type: "flash/storeFlashMessage",
-                          payload: {
-                            type: "error",
-                            flashMessage: json.message,
-                          },
-                        });
-                        dispatch({
-                          type: "auth/logout",
-                        });
+  useEffect(() => {
+    const fetchData = async (url: string) => {
+      let response = await fetch(url);
+      let json = await response.json();
+      if (json.status === 200) {
+        dispatch({
+          type: "auth/login",
+          payload: {
+            role: json.body.role,
+            id: json.body.id,
+          },
+        });
+      } else {
+        dispatch({
+          type: "auth/logout",
+        });
+      }
+    };
+    fetchData("/api/user/check");
+  }, [dispatch, router]);
 
-                        /* mutate("/api/user/check", {
+  let content;
+  if (isLog === false) {
+    content = (
+      <>
+        <div className={styles.header__log}>
+          <div
+            onClick={() => {
+              setDisplayLogMenu(!displayLogMenu);
+              if (isActive) {
+                dispatch({ type: "menu/closeMenu" });
+              }
+            }}
+            className={styles.header__log__div}
+          ></div>
+          <>
+            <div
+              className={`${styles.header__log__container} ${
+                displayLogMenu === true
+                  ? styles.header__log__container__show
+                  : styles.header__log__container__hide
+              }`}
+            >
+              <ul
+                onMouseLeave={() => setOnHoverLink(pathname)}
+                className={`${styles.header__log__ul}`}
+              >
+                <li
+                  onMouseOver={() => setOnHoverLink("/profile")}
+                  className={`${styles.header__log__li} ${
+                    styles.header__log__li__1
+                  } ${
+                    displayLogMenu === true
+                      ? styles.header__log__li__show
+                      : styles.header__log__li__hide
+                  }`}
+                >
+                  <Link
+                    className={styles.header__log__li__link}
+                    href="/profile"
+                    onClick={() => setDisplayLogMenu(false)}
+                  >
+                    <div
+                      className={`${styles.header__log__li__point} ${
+                        onHoverLink === "/profile"
+                          ? styles.header__log__li__point__show
+                          : styles.header__log__li__point__hide
+                      }`}
+                    ></div>
+                    Compte
+                  </Link>
+                </li>
+                <li
+                  onMouseOver={() => setOnHoverLink("/historique")}
+                  className={`${styles.header__log__li} ${
+                    styles.header__log__li__2
+                  } ${
+                    displayLogMenu === true
+                      ? styles.header__log__li__show
+                      : styles.header__log__li__hide
+                  }`}
+                >
+                  <Link
+                    className={styles.header__log__li__link}
+                    href="/historique"
+                    onClick={() => setDisplayLogMenu(false)}
+                  >
+                    <div
+                      className={`${styles.header__log__li__point} ${
+                        onHoverLink === "/historique"
+                          ? styles.header__log__li__point__show
+                          : styles.header__log__li__point__hide
+                      }`}
+                    ></div>
+                    Historique des rendez-vous
+                  </Link>
+                </li>
+                <li
+                  onMouseOver={() => setOnHoverLink("/rendez-vous")}
+                  className={`${styles.header__log__li} ${
+                    styles.header__log__li__3
+                  } ${
+                    displayLogMenu === true
+                      ? styles.header__log__li__show
+                      : styles.header__log__li__hide
+                  }`}
+                >
+                  <Link
+                    className={styles.header__log__li__link}
+                    href="/rendez-vous"
+                    onClick={() => setDisplayLogMenu(false)}
+                  >
+                    <div
+                      className={`${styles.header__log__li__point} ${
+                        onHoverLink === "/rendez-vous"
+                          ? styles.header__log__li__point__show
+                          : styles.header__log__li__point__hide
+                      }`}
+                    ></div>
+                    Mes rendez-vous
+                  </Link>
+                </li>
+                <li
+                  onMouseOver={() => setOnHoverLink("/logout")}
+                  className={`${styles.header__log__li} ${
+                    styles.header__log__li__4
+                  } ${
+                    displayLogMenu === true
+                      ? styles.header__log__li__show
+                      : styles.header__log__li__hide
+                  }`}
+                >
+                  <span
+                    className={styles.header__log__li__link}
+                    onClick={() => {
+                      const logout = async () => {
+                        let response = await fetch("/api/user/logout");
+                        let json = await response.json();
+                        console.log(json);
+                        if (json && json.status === 200) {
+                          dispatch({
+                            type: "flash/storeFlashMessage",
+                            payload: {
+                              type: "error",
+                              flashMessage: json.message,
+                            },
+                          });
+                          dispatch({
+                            type: "auth/logout",
+                          });
+
+                          /* mutate("/api/user/check", {
                           ...json,
                         });
                         cache.delete("/api/user/check"); */
-                        /* setTimeout(() => {
+                          /* setTimeout(() => {
                           window.location.reload();
                         }, 2000); */
-                      }
-                    };
+                        }
+                      };
 
-                    logout();
-                  }}
-                >
-                  <div
-                    className={`${styles.header__log__li__point} ${
-                      onHoverLink === "/logout"
-                        ? styles.header__log__li__point__show
-                        : styles.header__log__li__point__hide
-                    }`}
-                  ></div>
-                  Déconnection
-                </span>
-              </li>
-            </ul>
-          </div>
-        </>
-      </div>
-    </>
-  );
+                      logout();
+                    }}
+                  >
+                    <div
+                      className={`${styles.header__log__li__point} ${
+                        onHoverLink === "/logout"
+                          ? styles.header__log__li__point__show
+                          : styles.header__log__li__point__hide
+                      }`}
+                    ></div>
+                    Déconnection
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </>
+        </div>
+      </>
+    );
+  } else {
+    content = (
+      <button
+        type="button"
+        className={styles.header__login}
+        onClick={() => {
+          handlerClick();
+        }}
+      >
+        Se connecter
+      </button>
+    );
+  }
+
   /* if (isError) {
     content = <div>Erreur</div>;
   }

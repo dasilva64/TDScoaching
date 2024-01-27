7a609abf5fefd370c0798bcc97405b017b89ad63
thomasop <tdss33@hotmail.com>
2024-01-27 12:12:54 +0100

    Edit

--- src/app/contact/components/api/route.ts
+++ src/app/contact/components/api/route.ts
@@ -129,7 +129,7 @@ export async function POST(request: NextRequest) {
         accessToken: accessToken as string,
       },
       tls: {
-        rejectUnauthorized: true,
+        rejectUnauthorized: false,
       },
     });
 

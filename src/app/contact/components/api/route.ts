f2149753792ce3fb87b91af0d9e0dbd446e27141
thomasop <tdss33@hotmail.com>
2024-01-27 08:51:38 +0100

    Edit

--- src/app/contact/components/api/route.ts
+++ src/app/contact/components/api/route.ts
@@ -52,6 +52,45 @@ export async function POST(request: NextRequest) {
         pass: process.env.DB_TOM_PASSWORD,
       },
     });
+    let mailOptions = {
+      from: "thomasdasilva010@gmail.com",
+      to: "contact@tds-coachingdevie.fr",
+      subject: object,
+      html: `<!DOCTYPE html>
+                        <html lang="fr">
+                          <head>
+                            <title>tds coaching</title>
+                            <meta charset="UTF-8" />
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                            <title>Document</title>
+                          </head>
+                          <body>
+                            
+                            <div style="width: 100%">
+                              <div style="text-align: center">
+                                <img src="https://tdscoaching.fr/_next/image?url=%2Fassets%2Flogo%2Flogo3.webp&w=750&q=75" width="80px" height="80px" />
+                              </div>
+                              <div style="text-align: center; background: aqua; padding: 50px 0px; border-radius: 20px">
+                                <h1 style="text-align: center">tds coaching</h1>
+                                <h2 style="text-align: center">${validator.escape(
+                                  firstname.trim()
+                                )} ${validator.escape(
+        lastname.trim()
+      )} vous a envoyé un message</h2>
+                                <p style="text-align: left; margin-left: 20px">Email : ${validator.escape(
+                                  email.trim()
+                                )}</p>
+                                <p style="text-align: left; margin-left: 20px">Compte : l'utilisateur est inscrit</p>
+                                <p style="text-align: left; margin-left: 20px">Message : ${validator.escape(
+                                  message.trim()
+                                )}</p>
+                              </div>
+                            </div>
+                          </body>
+                        </html>`,
+    };
+    smtpTransport.sendMail(mailOptions);
     /* let smtpTransport = nodemailer.createTransport({
       host: "smtp.ionos.com",
       port: 465,

2e7f5864a1d23b3cca60f48d2f9163931e716608
thomasop <tdss33@hotmail.com>
2024-01-30 15:42:33 +0100

    Edit

--- next.config.js
+++ next.config.js
@@ -4,10 +4,32 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;`
+
 const nextConfig = {
   productionBrowserSourceMaps: true,
   async headers() {
     return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
       {
         // matching all API routes
         source: "/",

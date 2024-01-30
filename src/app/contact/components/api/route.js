d6eac6ed6a7b8c6a573597a5b8b4c25bc07671c9
thomasop <tdss33@hotmail.com>
2024-01-30 07:38:58 +0100

    Edit

--- src/app/contact/components/api/route.js
+++ src/app/contact/components/api/route.js
@@ -91,56 +91,6 @@ export async function POST(NextRequest) {
     const user = await prisma.user.findUnique({
       where: { mail: validator.escape(email.trim()), status: true },
     });
-    /* const oAuth2 = new OAuth2Client(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      "https://developers.google.com/oauthplayground"
-    );
-
-    oAuth2.setCredentials({
-      refresh_token: process.env.REFRESH_TOKEN,
-    });
-    const createTransporter = async () => {
-      const oauth2Client = new google.auth.OAuth2(
-        process.env.CLIENT_ID,
-        process.env.CLIENT_SECRET,
-        "https://developers.google.com/oauthplayground"
-      );
-
-      oauth2Client.setCredentials({
-        refresh_token: process.env.REFRESH_TOKEN,
-      });
-
-      const accessToken = await new Promise((resolve, reject) => {
-        oauth2Client.getAccessToken((err, token) => {
-          if (err) {
-            reject("Failed to create access token :(");
-          }
-          resolve(token);
-        });
-      });
-      const accessToken = await oauth2Client.getAccessToken();
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          type: "OAuth2",
-          user: process.env.EMAIL,
-          accessToken,
-          clientId: process.env.CLIENT_ID,
-          clientSecret: process.env.CLIENT_SECRET,
-          refreshToken: process.env.REFRESH_TOKEN,
-        },
-        tls: {
-          rejectUnauthorized: true
-        }
-      });
-
-      return transporter;
-    };
-    const sendEmail = async (emailOptions) => {
-      let emailTransporter = await createTransporter();
-      await emailTransporter.sendMail(emailOptions);
-    };*/
     let smtpTransport = nodemailer.createTransport({
       host: "smtp.ionos.fr",
       port: 465,
@@ -153,7 +103,7 @@ export async function POST(NextRequest) {
     if (user === null) {
       let mailOptions = {
         from: "contact@tds-coachingdevie.fr",
-        to: "thomasdasilva010@gmail.com",
+        to: "contact@tds-coachingdevie.fr",
         subject: object,
         html: `<!DOCTYPE html>
                           <html lang="fr">
@@ -189,11 +139,17 @@ export async function POST(NextRequest) {
                             </body>
                           </html>`,
       };
-      await smtpTransport.sendMail(mailOptions);
+      let test;
+      await smtpTransport.sendMail(mailOptions).then((res) => {
+        test = res;
+      }
+      ).catch((err) => {
+      test = err
+      });
     } else {
       let mailOptions = {
         from: "contact@tds-coachingdevie.fr",
-        to: "thomasdasilva010@gmail.com",
+        to: "contact@tds-coachingdevie.fr",
         subject: object,
         html: `<!DOCTYPE html>
                           <html lang="fr">
@@ -229,12 +185,53 @@ export async function POST(NextRequest) {
                             </body>
                           </html>`,
       };
-      await smtpTransport.sendMail(mailOptions);
+      let test;
+      await smtpTransport.sendMail(mailOptions).then((res) => {
+        test = res;
+      }
+      ).catch((err) => {
+      test = err
+      });
     }
-
+    /* let mailOptions = {
+      from: "contact@tds-coachingdevie.fr",
+      to: validator.escape(email.trim()),
+      subject: object,
+      html: `<!DOCTYPE html>
+                        <html lang="fr">
+                          <head>
+                            <title>tds coaching</title>
+                            <meta charset="UTF-8" />
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                            <title>Document</title>
+                          </head>
+                          <body>
+                            
+                            <div style="width: 100%">
+                              <div style="text-align: center">
+                                <img src="https://tdscoaching.fr/_next/image?url=%2Fassets%2Flogo%2Flogo3.webp&w=750&q=75" width="80px" height="80px" />
+                              </div>
+                              <div style="text-align: center; background: aqua; padding: 50px 0px; border-radius: 20px">
+                                <h1 style="text-align: center">tds coaching</h1>
+                                <h2 style="text-align: center">tds coaching a reçu votre message</h2>
+                                <p style="text-align: left; margin-left: 20px">Email : ${validator.escape(
+                                  email.trim()
+                                )}</p>
+                                <p style="text-align: left; margin-left: 20px">Compte : l'utilisateur est inscrit</p>
+                                <p style="text-align: left; margin-left: 20px">Message : ${validator.escape(
+                                  message.trim()
+                                )}</p>
+                              </div>
+                            </div>
+                          </body>
+                        </html>`,
+    };
+    await smtpTransport.sendMail(mailOptions); */
     return NextResponse.json({
       status: 200,
       body: user,
+      test: test,
       message:
         "Merci de nous avoir contacter nous allons vous répondre le plus vite possible",
     });

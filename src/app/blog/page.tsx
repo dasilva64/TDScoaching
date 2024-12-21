01e3e6fbd0f9c1bc0aefc12c09e99125390b9f38
thomas2 <thomas2@MacBook-Pro-de-thomas.local>
2024-12-21 11:29:43 +0100

    Edit

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -41,7 +41,7 @@ const getData = unstable_cache(
     return getArticleData;
   },
   ["articles"],
-  { revalidate: 3, tags: ["articles"] }
+  { revalidate: 3600, tags: ["articles"] }
 );
 
 const page = async () => {

0e9f5b7e0118ce08e384dde725f7db9725246e21
thomas2 <thomas2@MacBook-Pro-de-thomas.local>
2025-05-22 19:06:01 +0200

    Edit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const cspHeader = `
-    default-src 'self';
+    default-src 'self' 'unsafe-inline';
     script-src 'self' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;

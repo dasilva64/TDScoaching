8e45e09d5de8fbdd566d8eeaf478bf572acfd8a7
thomas2 <thomas2@MacBook-Pro-de-thomas.local>
2025-05-29 19:14:59 +0200

    Edit

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,7 @@ export async function middleware(request: NextRequest) {
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
-    font-src 'self';
+    font-src 'self' data:;
     object-src 'none';
     base-uri 'self';
     form-action 'self';

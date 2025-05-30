d42c2f29c54bf731893dfcf1344ec5d6a05c9657
thomas2 <thomas2@MacBook-Pro-de-thomas.local>
2025-05-30 14:35:44 +0200

    Edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.scss";
 import { Providers } from "./redux/provider";
 import Header from "./components/header/header";
 
+export const dynamic = "force-dynamic";
+
 export default function RootLayout({
   children,
 }: {

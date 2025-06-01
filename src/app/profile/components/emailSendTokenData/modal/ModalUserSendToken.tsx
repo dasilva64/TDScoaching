795f218aafe90e1609e1b19225c91575bec3075f
thomas2 <thomas2@MacBook-Pro-de-thomas.local>
2025-06-01 12:53:53 +0200

    Edit

--- src/app/profile/components/emailSendTokenData/modal/ModalUserSendToken.tsx
+++ src/app/profile/components/emailSendTokenData/modal/ModalUserSendToken.tsx
@@ -15,7 +15,7 @@ const ModalUserSendToken = ({ data: userData, mutate }: any) => {
   const { displayModalSendTokenEmail, inputEmail } = useSelector(
     (state: RootState) => state.ModalSendTokenEmail
   );
-  console.log(userData.body.email)
+  console.log("inputEmail", inputEmail)
   const { csrfToken } = useSelector(
     (state: RootState) => state.csrfToken
   );
